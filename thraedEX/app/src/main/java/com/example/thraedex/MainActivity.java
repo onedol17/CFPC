2681c69a8f6a40bce136418f485e4755ac0d4cac
onedol17 <itaemin152@gmail.com>
2023-09-09 14:17:34 +0900

    :memo:

--- thraedEX/app/src/main/java/com/example/thraedex/MainActivity.java
+++ thraedEX/app/src/main/java/com/example/thraedex/MainActivity.java
@@ -1,31 +1,125 @@
 package com.example.thraedex;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
+
+    private LocationManager locationManager;
+    private Location mLastlocation = null;
     int value = 0;
     String x = "0";
 
+    String y = "0";
+
+    int Speed;
+    List<Integer> listA = new ArrayList<Integer>();
+
     String logFlag;
     String getTime;
+    TextView test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //권한 체크
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        test= (TextView)findViewById(R.id.test);
+        Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (lastKnownLocation != null) {
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            String formatDate = sdf.format(new Date(lastKnownLocation.getTime()));
+        }
+        // GPS 사용 가능 여부 확인
+        boolean isEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+
+
         BackgroundThread thread = new BackgroundThread();
         thread.start();
-
         ViewThread thread2 = new ViewThread();
         thread2.start();
     }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        double deltaTime = 0;
+
+        //  getSpeed() 함수를 이용하여 속도를 계산
+        double getSpeed = Double.parseDouble(String.format("%.3f", location.getSpeed()));
+        String formatDate = sdf.format(new Date(location.getTime()));
+        boolean isStart = false;
+
+
+        Speed = (int) getSpeed;
+        test.setText(Speed);
+
+
+        if (mLastlocation != null) {
+            //시간 간격
+            deltaTime = (location.getTime() - mLastlocation.getTime()) / 1000.0;
+            // 속도 계산
+            double speed = mLastlocation.distanceTo(location) / deltaTime;
+
+        }
+        // 현재위치를 지난 위치로 변경
+        mLastlocation = location;
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        //권한 체크
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        // 위치정보 업데이트
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //권한이 없을 경우 최초 권한 요청 또는 사용자에 의한 재요청 확인
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION) &&
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // 권한 재요청
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+                return;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+                return;
+            }
+        }
+
+    }
+
     class ViewThread extends Thread {
         public void run() {
             while (true) {
@@ -35,6 +129,9 @@ public class MainActivity extends AppCompatActivity {
                 }
                 if (logFlag == "true") {
                     Log.d("VAL", "VALUE: " + String.valueOf(value));
+                    System.out.println(listA);
+
+
                     logFlag = "False";
                 }
             }
@@ -54,12 +151,20 @@ public class MainActivity extends AppCompatActivity {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss");
                 getTime = dateFormat.format(date);
 
+                if (!String.valueOf(getTime.charAt(0)).equals(y)) {
+                    listA.clear();
+                }
+
                 if (!String.valueOf(getTime.charAt(6)).equals(x)) {
                     value += 10;
+                    listA.add(Speed);
                     logFlag = "true";
                 }
 
+
+
                 x = String.valueOf(getTime.charAt(6));
+                y = String.valueOf(getTime.charAt(0));
             }
         }
     }
